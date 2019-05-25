a40adea70f68bbff98f8484cd53b18f9e437a16d
priyanka-47 <48121501+priyanka-47@users.noreply.github.com>
2019-05-25 22:54:51 +0530

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,7 @@
+package com.priya.service;
+
+public class UserService {
+	void save(User user);
+
+    User findByUsername(String username);
+}
---
+++ UserServiceImpl.java
@@ -0,0 +1,21 @@
+package service;
+
+public class UserServiceImpl implements UserService{
+	private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new HashSet<>(roleRepository.findAll()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
